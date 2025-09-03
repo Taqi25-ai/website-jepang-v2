f61b56b5287d05a5e364fe11f5eb5ef4a1b39406
Desi Bakar <desybakar12@gmail.com>
2025-09-03 12:57:25 +0000

    c

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,59 @@
-require("./polyfills"); // tambahkan ini
-const path = require("path")
-const { createFilePath } = require(`gatsby-source-filesystem`)
+// gatsby-node.js
 
+// Load polyfills supaya File, Blob, FileReader tersedia saat build
+if (typeof global.File === "undefined") {
+  global.File = class File {
+    constructor(parts = [], filename = '', options = {}) {
+      this.parts = parts;
+      this.name = filename;
+      this.type = options.type || '';
+      this.lastModified = options.lastModified || Date.now();
+      this.size = parts.reduce((acc, part) => {
+        if (typeof part === 'string') return acc + part.length;
+        if (part instanceof Uint8Array) return acc + part.byteLength;
+        return acc;
+      }, 0);
+    }
+  };
+}
+
+if (typeof global.Blob === "undefined") {
+  global.Blob = class Blob {
+    constructor(parts = [], options = {}) {
+      this.parts = parts;
+      this.type = options.type || '';
+      this.size = parts.reduce((acc, part) => {
+        if (typeof part === 'string') return acc + part.length;
+        if (part instanceof Uint8Array) return acc + part.byteLength;
+        return acc;
+      }, 0);
+    }
+  };
+}
+
+if (typeof global.FileReader === "undefined") {
+  global.FileReader = class FileReader {
+    readAsArrayBuffer(file) {
+      this.result = new Uint8Array(file.size).buffer;
+      if (this.onload) this.onload({ target: this });
+    }
+    readAsText(file) {
+      this.result = file.parts.join('');
+      if (this.onload) this.onload({ target: this });
+    }
+  };
+}
+
+// ===============================
+// Gatsby Node API
+// ===============================
+
+const path = require("path");
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
-  const { createPage } = actions
-
-  const blogList = path.resolve(`./src/templates/blog-list.js`)
+  const { createPage } = actions;
+  const blogList = path.resolve(`./src/templates/blog-list.js`);
 
   const result = await graphql(`
     {
@@ -23,45 +70,40 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
         }
       }
     }
-  `)
+  `);
 
-  // Handle errors
   if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
+    reporter.panicOnBuild(`Error while running GraphQL query.`);
+    return;
   }
 
-  // Create markdown pages
-  const posts = result.data.allMarkdownRemark.edges
-  let blogPostsCount = 0
+  const posts = result.data.allMarkdownRemark.edges;
+  let blogPostsCount = 0;
 
   posts.forEach((post, index) => {
-    const id = post.node.id
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    const next = index === 0 ? null : posts[index - 1].node
+    const id = post.node.id;
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node;
+    const next = index === 0 ? null : posts[index - 1].node;
 
     createPage({
       path: post.node.frontmatter.slug,
       component: path.resolve(
         `src/templates/${String(post.node.frontmatter.template)}.js`
       ),
-      // additional data can be passed via context
       context: {
         id,
         previous,
         next,
       },
-    })
+    });
 
-    // Count blog posts.
     if (post.node.frontmatter.template === "blog-post") {
-      blogPostsCount++
+      blogPostsCount++;
     }
-  })
+  });
 
-  // Create blog-list pages
-  const postsPerPage = 9
-  const numPages = Math.ceil(blogPostsCount / postsPerPage)
+  const postsPerPage = 9;
+  const numPages = Math.ceil(blogPostsCount / postsPerPage);
 
   Array.from({ length: numPages }).forEach((_, i) => {
     createPage({
@@ -73,18 +115,18 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
         numPages,
         currentPage: i + 1,
       },
-    })
-  })
-}
+    });
+  });
+};
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
+  const { createNodeField } = actions;
   if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    const slug = createFilePath({ node, getNode, basePath: `pages` });
     createNodeField({
       node,
       name: `slug`,
       value: slug,
-    })
+    });
   }
-}
+};
---
+++ gatsby-ssr.js
@@ -0,0 +1,7 @@
+// gatsby-ssr.js
+
+// Load polyfills untuk Node environment
+require("./polyfills");
+
+// Optional: Kalau mau, bisa tambah log supaya tau polyfill aktif
+console.log("Polyfills File, Blob, dan FileReader sudah di-load di SSR.");
